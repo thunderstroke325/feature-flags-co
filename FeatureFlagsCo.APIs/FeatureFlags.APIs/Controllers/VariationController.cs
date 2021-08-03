9a7a241594e6afe46a71d1ab49c43548f031fd93
Comiscience <68597908+cosmic-flood@users.noreply.github.com>
2021-08-03 19:42:30 +0800

    Backlogs/2127 (#6)
    * working
    
    * add multi option variation update api
    
    * updating
    
    * 完成了rollout percentage的初级算法
    
    * enrich test case for rollout percentage algorithm
    
    * updated rollout-algo.md
    
    * fixed isssue in rollout-algo.md
    
    * saving
    
    * updated
    
    * updated
    
    * integration test - updating
    
    * added postman integration test file for multi option get variation
    
    * a not bad version for integration test
    
    * do some update

--- FeatureFlagsCo.APIs/FeatureFlags.APIs/Controllers/VariationController.cs
+++ FeatureFlagsCo.APIs/FeatureFlags.APIs/Controllers/VariationController.cs
@@ -155,62 +155,7 @@ namespace FeatureFlags.APIs.Controllers
             }
         }
 
-        [HttpGet]
-        [Route("IfBelongRolloutPercentage")]
-        public string IfBelongRolloutPercentage()
-        {
-            string returnValue = "";
-            for(int k = 0; k < 10; k++)
-            {
-                returnValue += $"   ##Rround {k}## ";
-                int trueCount = 0;
-                for (int i = 0; i < 100; i++)
-                {
-                    if (_variationService.IfBelongRolloutPercentage(Guid.NewGuid().ToString(), new double[] { 0.0, 0.333 }))
-                        trueCount++;
-                }
-                returnValue += $" 100 sample (GUID): {trueCount};";
-                trueCount = 0;
-                for (int i = 0; i < 100; i++)
-                {
-                    if (_variationService.IfBelongRolloutPercentage($"hu-beau{1000*k+i}@outlook.com", new double[] { 0.0, 0.333 }))
-                        trueCount++;
-                }
-                returnValue += $" 100 sample (小变化邮箱): {trueCount};";
-
-
-                trueCount = 0;
-                for (int i = 0; i < 1000; i++)
-                {
-                    if (_variationService.IfBelongRolloutPercentage(Guid.NewGuid().ToString(), new double[] { 0.0, 0.333 }))
-                        trueCount++;
-                }
-                returnValue += $" 1000 sample (GUID): {trueCount};";
-                trueCount = 0;
-                for (int i = 0; i < 1000; i++)
-                {
-                    if (_variationService.IfBelongRolloutPercentage($"hu-beau{10000 * k + i}@outlook.com", new double[] { 0.0, 0.333 }))
-                        trueCount++;
-                }
-                returnValue += $" 1000 sample (小变化邮箱): {trueCount};";
-
-                trueCount = 0;
-                for (int i = 0; i < 10000; i++)
-                {
-                    if (_variationService.IfBelongRolloutPercentage(Guid.NewGuid().ToString(), new double[] { 0.0, 0.333 }))
-                        trueCount++;
-                }
-                returnValue += $" 10000 sample: {trueCount};";
-                trueCount = 0;
-                for (int i = 0; i < 10000; i++)
-                {
-                    if (_variationService.IfBelongRolloutPercentage($"hu-beau{100000 * k + i}@outlook.com", new double[] { 0.0, 0.333 }))
-                        trueCount++;
-                }
-                returnValue += $" 10000 sample (小变化邮箱): {trueCount};";
-            }
-            return returnValue;
-        }
+        
 
         [HttpPost]
         [Route("VariationResultTest")]
---
+++ FeatureFlagsCo.APIs/FeatureFlagsCo.APIs.Tests/RolloutPercentageTest.cs
@@ -0,0 +1,82 @@
+using FeatureFlags.APIs.Repositories;
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FeatureFlagsCo.APIs.Tests
+{
+    public class RolloutPercentageTest
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public RolloutPercentageTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Fact]
+        public void IfBelongRolloutPercentage()
+        {
+            var _variationService = new VariationService(null, null, null);
+            
+            for (int k = 0; k < 10; k++)
+            {
+                string returnValue = "";
+                returnValue += $"   ##Rround {k}## ";
+                int trueCount = 0;
+                for (int i = 0; i < 100; i++)
+                {
+                    if (_variationService.IfBelongRolloutPercentage(Guid.NewGuid().ToString(), new double[] { 0.0, 0.333 }))
+                        trueCount++;
+                }
+                returnValue += $" 100 sample (GUID): {trueCount};";
+                trueCount = 0;
+                for (int i = 0; i < 100; i++)
+                {
+                    if (_variationService.IfBelongRolloutPercentage($"hu-beau{1000 * k + i}@outlook.com", new double[] { 0.0, 0.333 }))
+                        trueCount++;
+                }
+                returnValue += $" 100 sample (小变化邮箱): {trueCount};";
+
+
+                trueCount = 0;
+                for (int i = 0; i < 1000; i++)
+                {
+                    if (_variationService.IfBelongRolloutPercentage(Guid.NewGuid().ToString(), new double[] { 0.0, 0.333 }))
+                        trueCount++;
+                }
+                returnValue += $" 1000 sample (GUID): {trueCount};";
+                trueCount = 0;
+                for (int i = 0; i < 1000; i++)
+                {
+                    if (_variationService.IfBelongRolloutPercentage($"hu-beau{10000 * k + i}@outlook.com", new double[] { 0.0, 0.333 }))
+                        trueCount++;
+                }
+                returnValue += $" 1000 sample (小变化邮箱): {trueCount};";
+
+                trueCount = 0;
+                for (int i = 0; i < 10000; i++)
+                {
+                    if (_variationService.IfBelongRolloutPercentage(Guid.NewGuid().ToString(), new double[] { 0.0, 0.333 }))
+                        trueCount++;
+                }
+                returnValue += $" 10000 sample: {trueCount};";
+                trueCount = 0;
+                for (int i = 0; i < 10000; i++)
+                {
+                    if (_variationService.IfBelongRolloutPercentage($"hu-beau{100000 * k + i}@outlook.com", new double[] { 0.0, 0.333 }))
+                        trueCount++;
+                }
+                returnValue += $" 10000 sample (小变化邮箱): {trueCount};";
+
+
+                _testOutputHelper.WriteLine(returnValue);
+            }
+        }
+    }
+}
