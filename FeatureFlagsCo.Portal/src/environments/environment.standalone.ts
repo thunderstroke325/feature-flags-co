9c859302136db362f81a5e29a5230204e2ce0c19
cosmos-explorer <88151306+cosmos-explorer@users.noreply.github.com>
2021-08-20 04:40:46 +0200

    Use variable to set portal api url (#33)

--- FeatureFlagsCo.Portal/src/environments/environment.standalone.ts
+++ FeatureFlagsCo.Portal/src/environments/environment.standalone.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   projectEnvKey: '#{PROJECT_ENV_KEY_PROD}#',
-  url: 'https://api.feature-flags.co',
+  url: '#{API_URL}#',
   name: 'Standalone'
 };
