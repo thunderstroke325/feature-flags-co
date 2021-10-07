d0bc94477614ca01582217eab65c8cdfa9f282b4
HONGYU <HongYu.0802@gmail.com>
2021-10-07 21:48:01 +0200

    fix trigger

--- FeatureFlagsCo.Portal/src/environments/environment.ts
+++ FeatureFlagsCo.Portal/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  projectEnvKey: '',
+  projectEnvKey: 'YjRlLWY1YjEtNCUyMDIxMDYwNzA2NTYwOF9fMl9fM19fN19fZGVmYXVsdF84NDNlMw==',
   url: 'https://ffc-api-ce2-dev.chinacloudsites.cn',
   name: 'Development',
   statisticUrl: null
