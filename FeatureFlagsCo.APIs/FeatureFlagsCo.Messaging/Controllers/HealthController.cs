4800ffbff720f0fa0d327a4ec13970dad8c7a405
Beau HU <hu-beau@outlook.com>
2021-10-13 06:48:58 +0800

    updated

---
+++ FeatureFlagsCo.APIs/FeatureFlagsCo.Messaging/Controllers/HealthController.cs
@@ -0,0 +1,32 @@
+﻿
+using FeatureFlagsCo.Messaging.Services;
+using FeatureFlagsCo.MQ;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FeatureFlagsCo.Messaging.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        public HealthController(
+            ServiceBusQ1Sender serviceBusQ1Sender,
+            ServiceBusQ5Sender serviceBusQ5Sender,
+            ServiceBusQ4Sender serviceBusSender)
+        {
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
