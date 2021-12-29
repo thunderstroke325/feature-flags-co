13bacf2b0e77c8048bf64160df0488614d6bb1f1
deleteLater <mikcczhang@gmail.com>
2021-12-30 00:15:17 +0800

    feat(swagger-api-doc): add public api authentication scheme in swagger

--- FeatureFlagsCo.APIs/FeatureFlags.APIs/Startup.cs
+++ FeatureFlagsCo.APIs/FeatureFlags.APIs/Startup.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Text;
 using FeatureFlags.APIs.Authentication;
 using FeatureFlags.APIs.Authentication.Scheme;
@@ -7,6 +5,7 @@ using FeatureFlags.APIs.Middlewares;
 using FeatureFlags.APIs.Repositories;
 using FeatureFlags.APIs.Services;
 using FeatureFlags.APIs.ViewModels;
+using FeatureFlags.Utils.ExtensionMethods;
 using FeatureFlagsCo.FeatureInsights;
 using FeatureFlagsCo.FeatureInsights.ElasticSearch;
 using FeatureFlagsCo.MQ.ElasticSearch;
@@ -20,7 +19,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 
 namespace FeatureFlags.APIs
 {
@@ -94,56 +92,7 @@ namespace FeatureFlags.APIs
             services.AddPublicApiAuthentication();
             #endregion
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "ToDo API",
-                    Description = "A simple example ASP.NET Core Web API",
-                    TermsOfService = new Uri("https://example.com/terms"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Shayne Boyer",
-                        Email = string.Empty,
-                        Url = new Uri("https://twitter.com/spboyer"),
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "Use under LICX",
-                        Url = new Uri("https://example.com/license"),
-                    }
-                });
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                        Reference = new OpenApiReference
-                            {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header
-                        },
-                        new List<string>()
-                        }
-                });
-            });
+            services.AddSwagger();
 
             services.AddScoped<IGenericRepository, GenericRepository<ApplicationDbContext>>();
             services.AddTransient<IEnvironmentUserPropertyService, EnvironmentUserPropertyService>();
---
+++ FeatureFlagsCo.APIs/FeatureFlags.Utils/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace FeatureFlags.Utils.ExtensionMethods
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "FeatureFlags API",
+                    Description = "backend api for feature flags",
+                });
+
+                options.AddJwtAuth();
+                options.AddPublicApiAuth();
+            });
+        }
+
+        public static SwaggerGenOptions AddJwtAuth(this SwaggerGenOptions options)
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n" +
+                              "Example Input: **Bearer the-token-string**",
+                Name = "Authorization",
+                Scheme = "Bearer",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+
+            return options;
+        }
+
+        public static SwaggerGenOptions AddPublicApiAuth(this SwaggerGenOptions options)
+        {
+            options.AddSecurityDefinition(RequestAuthKeys.EnvSecret, new OpenApiSecurityScheme
+            {
+                Name = RequestAuthKeys.EnvSecret,
+                Type = SecuritySchemeType.ApiKey,
+                In = ParameterLocation.Header,
+                Description = "envSecret Authorization for **Public Api**, fill your envSecret below."
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = RequestAuthKeys.EnvSecret
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+
+            return options;
+        }
+    }
+}
