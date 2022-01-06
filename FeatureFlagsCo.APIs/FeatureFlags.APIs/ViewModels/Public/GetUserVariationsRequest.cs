0bec25ef4dad6b5fe8cc98c0a544e4ae9aa24e26
deleteLater <mikcczhang@gmail.com>
2022-01-06 10:58:10 +0800

    fix(public-api): add parameter validation for feature-flag/variations api

--- FeatureFlagsCo.APIs/FeatureFlags.APIs/ViewModels/Public/GetUserVariationsRequest.cs
+++ FeatureFlagsCo.APIs/FeatureFlags.APIs/ViewModels/Public/GetUserVariationsRequest.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using FeatureFlags.APIs.Models;
 
 namespace FeatureFlags.APIs.ViewModels.Public
 {
     public class GetUserVariationsRequest
     {
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "the parameter 'userKeyId' cannot be null or empty")]
+        public string UserKeyId { get; set; }
+        
+        [Required(AllowEmptyStrings = false, ErrorMessage = "the parameter 'userName' cannot be null or empty")]
         public string UserName { get; set; }
 
         public string Email { get; set; }
-
-        public string UserKeyId { get; set; }
 
         public string Country { get; set; }
 
