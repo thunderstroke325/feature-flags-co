79467e44e65008d39477e5d50a7d1226c48f79f8
deleteLater <mikcczhang@gmail.com>
2022-02-28 14:30:01 +0800

    fix(sdk-streaming): handle connection close prematurely when close connection

--- FeatureFlagsCo.APIs/FeatureFlags.APIs/Middlewares/SdkWebSocketServerMiddleware.cs
+++ FeatureFlagsCo.APIs/FeatureFlags.APIs/Middlewares/SdkWebSocketServerMiddleware.cs
@@ -42,11 +42,23 @@ namespace FeatureFlags.APIs.Middlewares
             var sdkWebSocket = TryAcceptRequest(context);
             if (sdkWebSocket == null)
             {
-                await webSocket.CloseAsync(
-                    (WebSocketCloseStatus)4003,
-                    "invalid request, close by server",
-                    CancellationToken.None
-                );
+                try
+                {
+                    await webSocket.CloseAsync(
+                        (WebSocketCloseStatus)4003,
+                        "invalid request, close by server",
+                        CancellationToken.None
+                    );
+                }
+                catch (WebSocketException e) when (e.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely)
+                {
+                    webSocket.Abort();
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, ex.Message);
+                }
+                
                 return;
             }
 
@@ -123,12 +135,9 @@ namespace FeatureFlags.APIs.Middlewares
                         await wsService.OnMessageAsync(sdkWebSocket, message);
                     }
                 }
-                catch (WebSocketException e)
+                catch (WebSocketException e) when (e.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely)
                 {
-                    if (e.WebSocketErrorCode == WebSocketError.ConnectionClosedPrematurely)
-                    {
-                        socket.Abort();
-                    }
+                    socket.Abort();
                 }
                 catch (Exception ex)
                 {
