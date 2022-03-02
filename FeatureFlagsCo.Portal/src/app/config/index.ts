d7e7af99f0fb9dc1f05604ad8e382e6bae3fa894
deleteLater <mikcczhang@gmail.com>
2022-03-02 14:21:01 +0800

    chore: change document link

--- FeatureFlagsCo.Portal/src/app/config/index.ts
+++ FeatureFlagsCo.Portal/src/app/config/index.ts
@@ -1,10 +1,2 @@
 // 快速实战文档跳转链接
-export const QUICK_COMBAT_DOCUMENT = 'https://docs.feature-flags.co/';
-
-// 云服务商整合跳转链接
-export const INTEGRATION_OF_CLOUD_SERVICE_PROVIDERS = 'https://docs.feature-flags.co/opensource/sdk/';
-
-// 敏捷开关Bolierplate跳转链接
-export const AGILE_SWITCH_BOLIERPLATE = 'https://docs.feature-flags.co/feature-management/guide/';
-
-export const PHONE_NUMBER_REG = /^((\+?86)|(\(\+86\)))?1\d{10}$/;
+export const QUICK_COMBAT_DOCUMENT = 'https://minjiekaiguan.yuque.com/books/share/0096dd2d-8440-4b6c-982a-4b1009a38a66';
