7f2c7d18179fabb09d5e7ec530f7f9dd0beb0616
deleteLater <mikcczhang@gmail.com>
2022-03-03 12:57:25 +0800

    fix(user-management): change account service default version to v2

--- FeatureFlagsCo.Portal/src/app/services/account.service.ts
+++ FeatureFlagsCo.Portal/src/app/services/account.service.ts
@@ -14,7 +14,7 @@ export class AccountService {
   private _baseUrl: string;
   public get baseUrl(): string {
     if (!this._baseUrl) {
-      const apiVersion = this.ffcService.variation('backend-api-version', 'v1');
+      const apiVersion = this.ffcService.variation('backend-api-version', 'v2');
       this._baseUrl = `${environment.url}/api/${apiVersion}/accounts`;
     }
 
