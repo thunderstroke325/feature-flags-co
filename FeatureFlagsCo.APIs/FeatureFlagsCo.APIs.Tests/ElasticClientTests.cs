181f238402fd771f65511bd18725ca62fa78e01e
deleteLater <mikcczhang@gmail.com>
2022-03-14 09:40:07 +0800

    fix(infrastructure): aws open-search is not compatible with new version of elasticsearch-net package

---
+++ FeatureFlagsCo.APIs/FeatureFlagsCo.APIs.Tests/ElasticClientTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using FeatureFlags.APIs.Tests.TestBase;
+using Nest;
+using Shouldly;
+using Xunit;
+
+namespace FeatureFlags.APIs.Tests
+{
+    public class ElasticClientTests
+    {
+        private readonly ElasticClient _client = new ElasticClient(new Uri(TestConfigs.ElasticSearchUrl));
+
+        [Fact]
+        public async Task Should_Get_Indices()
+        {
+            var response = await _client.Cat.IndicesAsync();
+            response.IsValid.ShouldBeTrue();
+        }
+    }
+}
